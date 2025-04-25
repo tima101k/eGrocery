7ce8b2905a2a7d6253ee0f815d9a5bcc87c8fd1d
dikshyant <ulluxhunter@gmail.com>
2025-04-25 08:44:47 +0545

    update name

--- src/main/java/com/eGrocery/controller/CategoryController.java
+++ src/main/java/com/eGrocery/controller/CategoryController.java
@@ -26,7 +26,7 @@ public class CategoryController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/pages/category.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/pages/categories.jsp").forward(request, response);
 	}
 
 	/**
