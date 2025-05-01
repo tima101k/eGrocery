e3f22fabe622b9ce5730b7557e75f8e3dd98cbc1
tima101k <np01cp4a230335@islingtoncollege.edu.np>
2025-05-01 21:23:52 +0545

    add product

---
+++ src/main/java/com/eGrocery/model/ProductModel.java
@@ -0,0 +1,92 @@
+package com.eGrocery.model;
+
+public class ProductModel {
+	private Long id;
+	private String name;
+	private String image;
+	private String description;
+	private Long category;
+	private Float price;
+	private int stock_quantity;
+	private int sku;
+	private Float weight;
+	
+	public ProductModel(
+			Long id,
+			String name,
+			String image,
+			String description,
+			Long category,
+			Float price,
+			int stockQuantity,
+			int sku,
+			Float weight
+	) {
+		this.id = id;
+		this.name = name;
+		this.image = image;
+		this.description = description;
+		this.category = category;
+		this.stock_quantity = stockQuantity;
+		this.price = price;
+		this.sku = sku;
+		this.weight = weight;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getStockQuantity() {
+		return stock_quantity;
+	}
+	public void setStockQuantity(int stock_quantity) {
+		this.stock_quantity = stock_quantity;
+	}
+	public int getSku() {
+		return sku;
+	}
+	public void setSku(int sku) {
+		this.sku = sku;
+	}
+	public Float getWeight() {
+		return weight;
+	}
+	public void setWeight(Float weight) {
+		this.weight = weight;
+	}
+	public Long getCategory() {
+		return category;
+	}
+	public void setCategory(Long category) {
+		this.category = category;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+}
---
+++ src/main/java/com/eGrocery/service/ProductService.java
@@ -0,0 +1,96 @@
+package com.eGrocery.service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.eGrocery.config.Dbconfig;
+import com.eGrocery.model.CategoryModel;
+import com.eGrocery.model.ProductModel;
+
+public class ProductService {
+	
+	private Connection dbConn;
+	private boolean isConnectionError = false;
+	
+	public ProductService() {
+		try {
+			this.dbConn = Dbconfig.getDbConnection();
+		} catch (SQLException | ClassNotFoundException ex) {
+			System.err.println("Database connection error: " + ex.getMessage());
+			ex.printStackTrace();
+			isConnectionError = true;
+		}
+	}
+	
+	/**
+	 * Registers a new user in the database.
+	 *
+	 * @param studentModel the student details to be registered
+	 * @return Boolean indicating the success of the operation
+	 */
+	public Boolean addCategory(ProductModel productModel) {
+		if (isConnectionError) {
+			System.err.println("Database connection is not available.");
+			return null;
+		}
+		String insertQuery = "INSERT INTO products (name, image, description, category, price, stock_quantity, sku, weight) "
+				+ "VALUES (?, ?, ?, ?, ?, ?,?, ?)";
+		try {
+			PreparedStatement insertStmt = dbConn.prepareStatement(insertQuery);
+			// Insert category details
+			insertStmt.setString(1, productModel.getName());
+			insertStmt.setString(2, productModel.getImage());
+			insertStmt.setString(3, productModel.getDescription());
+			insertStmt.setLong(4, productModel.getCategory());
+			insertStmt.setFloat(5, productModel.getPrice());
+			insertStmt.setInt(6, productModel.getStockQuantity());
+			insertStmt.setLong(7, productModel.getSku());
+			insertStmt.setFloat(8, productModel.getWeight());
+			
+			return insertStmt.executeUpdate() > 0;
+		} catch (SQLException e) {
+			System.err.println("Error during user registration: " + e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public List<ProductModel> getAllProducts() {
+		if (isConnectionError) {
+			System.err.println("Database connection is not available.");
+			return null;
+		}
+
+		// SQL query to fetch details
+		String query = "SELECT * from categories";
+		try (PreparedStatement stmt = dbConn.prepareStatement(query)) {
+			ResultSet result = stmt.executeQuery();
+			List<ProductModel> products = new ArrayList<>();
+			while (result.next()) {
+				// Create and add StudentModel to the list
+				products.add(
+					new ProductModel(
+						result.getLong("id"),
+						result.getString("name"), 
+						result.getString("image"),
+						result.getString("description"), 
+						result.getLong("category"),
+						result.getFloat("price"),
+						result.getInt("stock_quantity"),
+						result.getInt("sku"),
+						result.getFloat("weight")
+				));
+			}
+			return products;
+		} catch (SQLException e) {
+			// Log and handle exceptions related to student query execution
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
