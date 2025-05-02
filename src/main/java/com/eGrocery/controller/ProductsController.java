dc82633c38e995fb1819a206618f1df1ec866469
tima101k <np01cp4a230335@islingtoncollege.edu.np>
2025-05-02 08:39:50 +0545

    add product description controller

---
+++ src/main/java/com/eGrocery/controller/ProductsController.java
@@ -0,0 +1,44 @@
+package com.eGrocery.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.eGrocery.service.ProductService;
+
+/**
+ * Servlet implementation class ProductController
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/products" })
+public class ProductsController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	ProductService productService = new ProductService();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductsController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("productList", productService.getAllProducts());
+		request.getRequestDispatcher("/WEB-INF/pages/products.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
