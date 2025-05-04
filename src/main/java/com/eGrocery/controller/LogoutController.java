d6e40f6b18585a6a35dcf5d46e4c5484f6e0a55a
tima101k <np01cp4a230335@islingtoncollege.edu.np>
2025-05-04 11:10:23 +0545

    add logout

---
+++ src/main/java/com/eGrocery/controller/LogoutController.java
@@ -0,0 +1,35 @@
+package com.eGrocery.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import com.eGrocery.utils.SessionUtil;
+
+/**
+ * Servlet implementation class LogoutController
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/logout" })
+public class LogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	SessionUtil sessionUtil = new SessionUtil();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		SessionUtil.invalidateSession(request);
+		request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+	}
+}
